752c604852721dfbd53339e0dd79189f2c406490
jorgezafra94 <947@holbertonschool.com>
2019-12-17 23:47:43 +0000

    [update] change for back_populate and adding attribute place_amenities

--- models/amenity.py
+++ models/amenity.py
@@ -13,3 +13,7 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        from models.place import Place, place_amenity
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       viewonly=False)
