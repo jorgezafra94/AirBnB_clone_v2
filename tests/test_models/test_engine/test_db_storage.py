57e514ba037147db7bf5d1d0c3a32aacdcbd35b0
jorgezafra94 <947@holbertonschool.com>
2019-12-19 16:11:01 +0000

    [update] using setup and teardown methods

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -20,40 +20,24 @@ class TestDBStorage(unittest.TestCase):
     '''this will test the DBStorage'''
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(self):
         """set up for test"""
-        cls.storage = DBStorage()
-        # reload to get the HBNB_ENV = test and erase the existing ones
-        cls.storage.reload()
-        cls.state = State()
-        cls.state.name = "Santander"
-        cls.state.save()
-        cls.city = City()
-        cls.city.state_id = cls.state.id
-        cls.city.name = "Velez"
-        cls.city.save()
-        cls.user = User()
-        cls.user.first_name = "Jhonathan"
-        cls.user.last_name = "Pericles"
-        cls.user.password = "solo Perez"
-        cls.user.email = "1234@yahoo.com"
-        cls.user.save()
-        cls.place = Place()
-        cls.place.city_id = cls.city.id
-        cls.place.user_id = cls.user.id
-        cls.place.name = "Lovely_place"
-        cls.place.number_rooms = 3
-        cls.place.number_bathrooms = 1
-        cls.place.max_guest = 6
-        cls.place.price_by_night = 120
-        cls.place.latitude = 37.773972
-        cls.place.longitude = -122.431297
-        cls.place.save()
-        cls.review = Review()
-        cls.review.place_id = cls.place.id
-        cls.review.user_id = cls.user.id
-        cls.review.text = "Amazing_place,_huge_kitchen"
-        cls.review.save()
+        self.User = getenv("HBNB_MYSQL_USER")
+        self.Passwd = getenv("HBNB_MYSQL_PWD")
+        self.Db = getenv("HBNB_MYSQL_DB")
+        self.Host = getenv("HBNB_MYSQL_HOST")
+        self.db = MySQLdb.connect(host=self.Host, user=self.User,
+                                  passwd=self.Passwd, db=self.Db,
+                                  charset="utf8")
+        self.query = self.db.cursor()
+        self.storage = DBStorage()
+        self.storage.reload()
+
+    @classmethod
+    def teardown(self):
+        """at the end of the test this will tear it down"""
+        self.query.close()
+        self.db.close()
 
     @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
     def test_pep8_DBStorage(self):
@@ -63,137 +47,36 @@ class TestDBStorage(unittest.TestCase):
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_all_DBStorage(self):
-        """Test same size between storage() and existing db"""
-        User = getenv("HBNB_MYSQL_USER")
-        Passwd = getenv("HBNB_MYSQL_PWD")
-        Db = getenv("HBNB_MYSQL_DB")
-        Host = getenv("HBNB_MYSQL_HOST")
-        # change the HBNB_ENV in order to no erase more
-        # and use the tables created in SetUpClass
-        os.environ['HBNB_ENV'] = 'No_erase'
-        storage = DBStorage()
-        storage.reload()
-        db = MySQLdb.connect(host=Host, user=User,
-                             passwd=Passwd, db=Db,
-                             charset="utf8")
-        query = db.cursor()
-        dic = storage.all()
-        lis = []
-        query.execute("SHOW TABLES")
-        output = query.fetchall()
-        if len(output) != 0:
-            for elem in output:
-                if elem[0] != "place_amenity":
-                    query.execute("SELECT * FROM {}".format(elem[0]))
-                    salida = query.fetchall()
-                    if len(salida) != 0:
-                        for obj in salida:
-                            lis.append(obj)
-            self.assertEqual(len(dic), len(lis))
-        else:
-            self.assertEqual(len(dic), 0)
-        query.close()
-        db.close()
+    def test_read_tables(self):
+        """existing tables"""
+        self.query.execute("SHOW TABLES")
+        salida = self.query.fetchall()
+        self.assertEqual(len(salida), 7)
 
     @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_quantity_amenities(self):
-        """Test same size between storage(amenities) and existing db"""
-        User = getenv("HBNB_MYSQL_USER")
-        Passwd = getenv("HBNB_MYSQL_PWD")
-        Db = getenv("HBNB_MYSQL_DB")
-        Host = getenv("HBNB_MYSQL_HOST")
-        os.environ['HBNB_ENV'] = 'No_erase'
-        storage = DBStorage()
-        storage.reload()
-        db = MySQLdb.connect(host=Host, user=User,
-                             passwd=Passwd, db=Db,
-                             charset="utf8")
-        query = db.cursor()
-        dic = storage.all(Amenity)
-        lis = []
-        query.execute("SHOW TABLES")
-        output = query.fetchall()
-        if len(output) != 0:
-            for elem in output:
-                if elem[0] == "amenities":
-                    query.execute("SELECT * FROM {}".format(elem[0]))
-                    salida = query.fetchall()
-                    if len(salida) != 0:
-                        for obj in salida:
-                            lis.append(obj)
-            self.assertEqual(len(dic), len(lis))
-        else:
-            self.assertEqual(len(dic), 0)
-        query.close()
-        db.close()
+    def test_no_element_user(self):
+        """no elem in users"""
+        self.query.execute("SELECT * FROM users")
+        salida = self.query.fetchall()
+        self.assertEqual(len(salida), 0)
 
     @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_quantity_city(self):
-        """Test same size between storage(city) and existing db"""
-        User = getenv("HBNB_MYSQL_USER")
-        Passwd = getenv("HBNB_MYSQL_PWD")
-        Db = getenv("HBNB_MYSQL_DB")
-        Host = getenv("HBNB_MYSQL_HOST")
-        os.environ['HBNB_ENV'] = 'No_erase'
-        storage = DBStorage()
-        storage.reload()
-        db = MySQLdb.connect(host=Host, user=User,
-                             passwd=Passwd, db=Db,
-                             charset="utf8")
-        query = db.cursor()
-        dic = storage.all(City)
-        lis = []
-        query.execute("SHOW TABLES")
-        output = query.fetchall()
-        if len(output) != 0:
-            for elem in output:
-                if elem[0] == "cities":
-                    query.execute("SELECT * FROM {}".format(elem[0]))
-                    salida = query.fetchall()
-                    if len(salida) != 0:
-                        for obj in salida:
-                            lis.append(obj)
-            self.assertEqual(len(dic), len(lis))
-        else:
-            self.assertEqual(len(dic), 0)
-        query.close()
-        db.close()
+    def test_no_element_cities(self):
+        """no elem in cities"""
+        self.query.execute("SELECT * FROM cities")
+        salida = self.query.fetchall()
+        self.assertEqual(len(salida), 0)
 
     @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
     def test_add(self):
-        """Test same size between storage(city) and existing db"""
-        User = getenv("HBNB_MYSQL_USER")
-        Passwd = getenv("HBNB_MYSQL_PWD")
-        Db = getenv("HBNB_MYSQL_DB")
-        Host = getenv("HBNB_MYSQL_HOST")
-        os.environ['HBNB_ENV'] = 'No_erase'
-        storage = DBStorage()
-        storage.reload()
-        db = MySQLdb.connect(host=Host, user=User,
-                             passwd=Passwd, db=Db,
-                             charset="utf8")
-        query = db.cursor()
-        dic = storage.all(State)
-        lis = []
-        query.execute("INSERT INTO states (id, created_at, updated_at, name)\
+        """Test same size between storage() and existing db"""
+        self.query.execute("INSERT INTO states (id, created_at, updated_at, name)\
                       VALUES (578, '2019-05-11 00:14:40',\
                       '2019-05-11 00:14:40', '{}')".format("boyaca"))
-        query.execute("SHOW TABLES")
-        output = query.fetchall()
-        if len(output) != 0:
-            for elem in output:
-                if elem[0] == "states":
-                    query.execute("SELECT * FROM {}".format(elem[0]))
-                    salida = query.fetchall()
-                    if len(salida) != 0:
-                        for obj in salida:
-                            lis.append(obj)
-            self.assertEqual(len(dic) + 1, len(lis))
-        else:
-            self.assertEqual(len(dic), 0)
-        query.close()
-        db.close()
+
+        self.query.execute("SELECT * FROM {}".format("states"))
+        salida = self.query.fetchall()
+        self.assertEqual(len(salida), 1)
 
 if __name__ == "__main__":
     unittest.main()
