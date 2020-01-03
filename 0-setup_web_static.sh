50cedb88a5297d5b7fe0217da4953fbe9def1834
jorgezafra94 <947@holbertonschool.com>
2020-01-03 20:57:40 +0000

    [fix] script configuration to serve static pages

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,6 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 # enable pass of traffic in the port 80
 sudo ufw allow 'Nginx HTTP'
-sudo service nginx start
 # instructions
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
@@ -11,10 +10,10 @@ sudo touch /data/web_static/releases/test/index.html
 cd /data/web_static/releases/test/
 echo -e "<html>\n  <head>\n  </head>\n  <body>\n    FAKE HTML\n  </body>\n</html>" > index.html
 cd -
-sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo ln -fs /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 cd /etc/nginx/sites-available/
-sudo sed -i "s/404_page.html;/404_page.html;\n\t# static page/" default
-sudo sed -i "s/# static page/# static page\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/index.html;\n\t}/" default
+sudo sed -i "s/404_page.html;/404_page.html;\n\n\t# static page/" default
+sudo sed -i "s/# static page/# static page\n\n\tlocation \/hbnb_static\/index.html {\n\t\talias \/data\/web_static\/current\/index.html;\n\t}/" default
 cd -
 sudo service nginx restart
