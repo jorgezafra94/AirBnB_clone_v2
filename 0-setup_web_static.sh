dc28fccbbbbce6965b0270106181b701391e01c4
jorgezafra94 <947@holbertonschool.com>
2020-01-03 22:27:21 +0000

    [fix] script configuration to serve static pages

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 # script to configure in order to serve the static pages
-sudo apt-get -y update
-sudo apt-get -y install nginx
-# enable pass of traffic in the port 80
-sudo ufw allow 'Nginx HTTP'
-name=$HOSTNAME
-# configure the file nginx.conf in order the set the header
-sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\t# Adding Header/" /etc/nginx/nginx.conf
-sudo sed -i "s/# Adding Header/# Adding Header \n\tadd_header X-Served-By $name;/" /etc/nginx/nginx.conf
-# instructions
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "<html>
   <head>
   </head>
@@ -17,11 +11,8 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-cd /etc/nginx/sites-available/
-sudo sed -i "s/404_page.html;/404_page.html;\n\n\t# static page/" default
-sudo sed -i "s/# static page/# static page\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex on;\n\t}\n/" default
-cd -
-sudo service nginx restart
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\tlocation \/hbnb_static\/ { alias \/data\/web_static\/current/;}\n" /etc/nginx/sites-available/default
+service nginx restart
 exit 0
