334431e463dc40aa8d6539e0395b5c10d66f0d0c
jorgezafra94 <947@holbertonschool.com>
2020-01-03 21:59:37 +0000

    [fix] script configuration to serve static pages

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,11 +9,17 @@ name=$HOSTNAME
 sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\t# Adding Header/" /etc/nginx/nginx.conf
 sudo sed -i "s/# Adding Header/# Adding Header \n\tadd_header X-Served-By $name;/" /etc/nginx/nginx.conf
 # instructions
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 cd /data/web_static/releases/test/
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 cd -
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
