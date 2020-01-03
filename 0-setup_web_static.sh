7e628f77c31eb0bf03bcf89e6c6bb72e19fce531
jorgezafra94 <947@holbertonschool.com>
2020-01-03 21:38:34 +0000

    [fix] script configuration to serve static pages

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,11 +16,11 @@ sudo touch /data/web_static/releases/test/index.html
 cd /data/web_static/releases/test/
 echo -e "<html>\n  <head>\n  </head>\n  <body>\n    FAKE HTML\n  </body>\n</html>" > index.html
 cd -
-sudo ln -fs /data/web_static/releases/test /data/web_static/current
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 cd /etc/nginx/sites-available/
 sudo sed -i "s/404_page.html;/404_page.html;\n\n\t# static page/" default
-sudo sed -i "s/# static page/# static page\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}/" default
+sudo sed -i "s/# static page/# static page\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n/" default
 cd -
 sudo service nginx restart
 exit 0
