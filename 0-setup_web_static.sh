ea57a3f6c0d540b3fc13ded1fb0d8657afad474b
jorgezafra94 <947@holbertonschool.com>
2020-01-03 21:56:49 +0000

    [fix] script configuration to serve static pages

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,19 +8,18 @@ name=$HOSTNAME
 # configure the file nginx.conf in order the set the header
 sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\t# Adding Header/" /etc/nginx/nginx.conf
 sudo sed -i "s/# Adding Header/# Adding Header \n\tadd_header X-Served-By $name;/" /etc/nginx/nginx.conf
-sudo service nginx restart
 # instructions
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
 cd /data/web_static/releases/test/
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    FAKE HTML\n  </body>\n</html>" > index.html
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > index.html
 cd -
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 cd /etc/nginx/sites-available/
 sudo sed -i "s/404_page.html;/404_page.html;\n\n\t# static page/" default
-sudo sed -i "s/# static page/# static page\n\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n/" default
+sudo sed -i "s/# static page/# static page\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex on;\n\t}\n/" default
 cd -
 sudo service nginx restart
 exit 0
