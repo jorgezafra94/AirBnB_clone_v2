91cadb68afb31f06b1e59a0d3bf89140f0029384
jorgezafra94 <947@holbertonschool.com>
2020-01-04 04:32:35 +0000

    [feature] puppet point 0

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,15 +7,15 @@ exec {'install-Nginx':
 }
 
 exec {'create folders':
-  command  => 'sudo mkdir -p /data/web_static/releases/test/ ; sudo mkdir -p /data/web_static/shared/',
+  command  => 'sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/',
   provider => shell,
   before   => Exec['create file'],
 }
 
 exec {'create file':
-  command  => 'echo "Fake content" | sudo tee  /data/web_static/releases/test/index.html',
-  provider => shell,
-  before   => Exec['symbolic'],
+  command => 'echo "Fake content" | sudo tee  /data/web_static/releases/test/index.html',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  before  => Exec['symbolic'],
 }
 
 exec {'symbolic':
@@ -31,13 +31,12 @@ exec {'owner':
 }
 
 exec {'content':
-  command  => 'sudo sed -i '43i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;
-\n\t autoindex on;\n\t}\n' /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i '43i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t autoindex on;\n\t}\n' /etc/nginx/sites-available/default',
   provider => shell,
   before   => Exec['config'],
 }
 
 exec {'config':
-  command  => 'sudo service nginx restart; exit 0',
-  provider =>shell,
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
